577c71cea2c29262958935cfc9c1d1447f571801
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-10 11:55:08 +0300

    Styled the react component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -7,17 +7,20 @@ const MealItem = (props) =>
     const price = `$${props.price.toFixed(2)}`
 
     return (
-        <li>
+        <li className={classes.meal}>
             <div>
                 <h3>
                     {props.name}
                 </h3>
-                <div>
+                <div className={classes.description}>
                     {props.description}
                 </div>
-                <div>
+                <div className={classes.price}>
                     {price}
                 </div>
+            </div>
+            <div>
+
             </div>
         </li>
     )
