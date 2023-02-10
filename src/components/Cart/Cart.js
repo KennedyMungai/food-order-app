5ccf4abd64ffbefaee4fc28315389e014322f5fd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-10 14:38:40 +0300

    Made the close button close the modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -26,7 +26,10 @@ const Cart = (props) =>
                 <span>35.62</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes['button--alt']}>
+                <button
+                    className={classes['button--alt']}
+                    onClick={props.onClose}
+                >
                     Close
                 </button>
                 <button className={classes.button}>
