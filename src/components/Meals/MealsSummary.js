f04eb6c319576d2123b50b04db3c0208bd9dcc3c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-10 11:19:47 +0300

    Imported styles for the MealsSummary component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () =>
+{
+    return (
+        <div>MealsSummary</div>
+    )
+}
+
+export default MealsSummary
