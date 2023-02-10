6c05fb6597986bfd0000d4f402f70a2fcf88e592
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-10 11:15:05 +0300

    Styled the header cart button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import classes from './Header.module.css'
 import mealsImage from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
 
 const Header = (props) =>
 {
@@ -9,7 +10,7 @@ const Header = (props) =>
         <>
             <header className={classes.header}>
                 <h1>Meals R Us</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
                 <img
