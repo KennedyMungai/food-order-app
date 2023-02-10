740648e1667d7f163717625bf3f17515a66d6186
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-10 13:06:24 +0300

    Created both the backdrop and the ModalOverlay components

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 import classes from './Modal.module.css'
 
+const Backdrop = props => { }
+
+const ModalOverlay = props => { }
+
 const Modal = (props) =>
 {
     return (
