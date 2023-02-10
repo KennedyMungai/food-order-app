99d941b9d32683d20dcf3debf3b151d0055be331
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-10 15:38:39 +0300

    Implemented numberOfCartItems logic

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -8,7 +8,10 @@ const HeaderCartButton = (props) =>
 {
     const cartCtx = useContext(CartContext)
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => { }, 0)
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) =>
+    {
+        return curNumber + item.amount
+    }, 0)
 
     return (
         <button className={classes.button} onClick={props.onClick}>
@@ -19,7 +22,7 @@ const HeaderCartButton = (props) =>
                 Your Cart
             </span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     )
