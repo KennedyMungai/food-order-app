e04e8d534f9eaff92b13925b48d8209dee348192
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-13 11:42:59 +0300

    Wired up the ref to the useRef hook instance

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -14,14 +14,18 @@ const MealItemForm = (props) =>
 
     return (
         <form action="" className={classes.form} onSubmit={submitHandler}>
-            <Input label='Amount' input={{
-                id: 'amount_' + props.id,
-                type: 'number',
-                min: '1',
-                max: '5',
-                step: '1',
-                defaultValue: '1'
-            }} />
+            <Input
+                label='Amount'
+                ref={amountInputRef}
+                input={{
+                    id: 'amount_' + props.id,
+                    type: 'number',
+                    min: '1',
+                    max: '5',
+                    step: '1',
+                    defaultValue: '1'
+                }}
+            />
             <button>
                 + Add
             </button>
