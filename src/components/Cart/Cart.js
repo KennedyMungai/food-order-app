cec7c95fa1cc030367c5ab5094609d1328c6225e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-14 14:51:17 +0300

    Made total amount a string literal containing a literal value

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,7 +8,7 @@ const Cart = (props) =>
 {
     const cartCtx = useContext(CartContext)
 
-    const totalAmount = cartCtx.totalAmount.toFixed(2)
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
 
     const cartItems = <ul className={classes['cart-items']}>
         {cartCtx.items.map(item => <li>{item.name}</li>)}
